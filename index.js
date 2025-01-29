2570f44f4a586062106842aff7984673acf499b0
Kunal <kunal.singh2018@vitstudent.ac.in>
2025-01-29 18:35:41 +0400

    try

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 // Enable CORS for all origins
 
 const corsOptions = {
-  origin: "*", // Change this to your frontend URL in production
+  origin: "http://localhost:3000/", // Change this to your frontend URL in production
   methods: "GET,POST,PUT,DELETE,OPTIONS",
   allowedHeaders: "Content-Type, Authorization, x-client-api-key",
 };
